4ad0d7563e56002c23c2204b67e8bde7b1c57f8a
LuanLudwig <luantelisluh@gmail.com>
2024-03-06 17:04:25 -0300

    second commit

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
+const { default: mongoose} = require("mongoose");
 
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -46,6 +47,101 @@ const register = async (req, res) => {
     });
 };
 
+const login = async (req, res) => {
+    
+    const { email, password } = req.body;
+    
+    const user = await User.findOne({email})
+
+    //check if user exists
+    if(!user) {
+        res.status(404).json({error: ["Usuário não encontrado."]})
+        return
+    }
+
+    // check if password matches
+    if(!(await bcrypt.compare(password, user.password))) {
+        res.status(422).json({errors: ["Senha inválida."]})
+        return
+    };
+
+    //Return user with token
+    res.status(201).json({
+        _id: user._id,
+        profileImage: user.profileImage,
+        token: generateToken(user._id)
+    });
+};
+
+// Get current logged in user
+const getCurrentUser = async (req, res) => {
+    const user = req.user
+
+    res.status(200).json(user)
+};
+
+// Update an user
+const update = async (req, res) => {
+    const { name, password, bio } = req.body;
+    
+    let profileImage = null;
+
+    if(req.file) {
+        profileImage = req.file.filename;
+    }
+
+    const reqUser = req.user;
+
+    const user = await User.findById(mongoose.Types.ObjectId(reqUser._id)).select(
+        "-password"
+      );
+    
+    if(name) {
+        user.name = name
+    }
+
+    if (password) {
+        const salt = await bcrypt.gentSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+        user.password = passwordHash
+    }
+
+    if (profileImage) {
+        user.profileImage = profileImage
+    }
+
+    if(bio) {
+        user.bio = bio;
+    }
+
+    await user.save();
+
+    res.status(200).json(user)
+};
+
+const getUserById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const user = await User.findById(mongoose.Types.ObjectId(id)).select("-password");
+
+        // Check id user exists
+        if(!user) {
+            res.status(404).json({ errors: ["Usuário não encontrado!"]});
+            return
+        }
+
+        res.status(200).json(user)
+
+    } catch (error) {
+        res.status(422).json({ errors: ["Usuário não encontrado!"]})
+    }
+ }
+
 module.exports = {
     register,
+    login,
+    getCurrentUser,
+    update,
+    getUserById
 };
