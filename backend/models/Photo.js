338396e817a0b7672d957af14931c92abcf9a23a
LuanLudwig <luantelisluh@gmail.com>
2024-03-07 15:58:43 -0300

    insert photos

--- backend/models/Photo.js
+++ backend/models/Photo.js
@@ -2,19 +2,19 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const photoSchema = new Schema(
-    {
-        image: String,
-        title: String,
-        likes: Array,
-        comments: Array,
-        userId: mongoose.ObjectId,
-        userName: String,
-    },
-    {
-        timestamps: true
-    }
+  {
+    image: String,
+    title: String,
+    likes: Array,
+    comments: Array,
+    userId: mongoose.ObjectId,
+    userName: String,
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const Photo = mongoose.model("photo", photoSchema);
+Photo = mongoose.model("Photo", photoSchema);
 
 module.exports = Photo;
---
+++ backend/routes/PhotoRoutes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+
+const { insertPhoto, deletePhoto } = require('../controllers/PhotoController');
+
+const { photoInsertValidation } = require('../middlewares/photoValidation');
+const authGuard = require('../middlewares/authGuard');
+const validate = require('../middlewares/handleValidation');
+const { imageUpload } = require('../middlewares/imageUpload');
+
+
+
+
+// Routes
+router.post(
+        "/", 
+        authGuard, 
+        imageUpload.single("image"),
+        photoInsertValidation(),
+        validate,
+        insertPhoto
+    );
+router.delete("/:id", authGuard, deletePhoto)
+
+
+
+
+
+
+
+
+
+module.exports = router
